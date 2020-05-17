2dca163b658827fdfcdd16126a6288e64b141ffc
Lukasz <lukaszkkoziel@gmail.com>
2020-05-17 16:26:14 +0900

    first model created - author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,11 +8,17 @@ class AuthorsController < ApplicationController
   end
 
   def show
-    # @author = Author.find(params[:id])
+     @author = Author.find(params[:id])
 
   end
 
   def create
+    @author = Author.new(author_params)
+    if @author.save
+      redirect_to @author, notice: 'Author was successfully created.'
+    else
+      render :new
+    end
 
   end
 
@@ -25,12 +31,14 @@ class AuthorsController < ApplicationController
   end
 
   def destroy
-
+    @author = Author.find(params[:id])
+    @author.destroy
+    redirect_to authors_path
   end
   private
 
   def author_params
-
+    params.require(:author).permit(:name)
   end
 
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -2,6 +2,6 @@ class Author < ApplicationRecord
   has_many :books
   validates :name, uniqueness: true
   validates :name, presence: true
-   validates :book, uniqueness: true
-  validates :book, presence: true
+  #  validates :book, uniqueness: true
+  # validates :book, presence: true
 end
