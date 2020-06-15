9d459c10d4ee2ca895779060e074e9681352e703
Lukasz <lukaszkkoziel@gmail.com>
2020-06-15 12:21:11 +0900

    creating instances to pass JSON

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,62 @@
 class BooksController < ApplicationController
 
-  def index
-    response = Book.__elasticsearch__.search(
-      query: {
-        multi_match: {
-          query: params[:query],
-          fields: ['name', 'author.first_name', 'author.last_name', 'isbn']
-        }
-      }
-    ).results
+  # def index
+  #   # response = Book.__elasticsearch__.search(
+  #   #   query: {
+  #   #     multi_match: {
+  #   #       query: params[:query],
+  #   #       fields: ['name', 'author.first_name', 'author.last_name', 'isbn']
+  #   #     }
+  #   #   }
+  #   # ).results
 
-    render json: {
-      results: response.results,
-      total: response.total
-    }
+  #   # render json: {
+  #   #   results: response.results,
+  #   #   total: response.total
+  #   # }
+  # end
+
+  def index
+    @books = Book.all
+    @search = params["search"]
+    if @search.present?
+      @title = @search["title"]
+      @books = Book.where(title: @title)
+    end
+  end
+
+  def search
+   url = "https://www.googleapis.com/books/v1/volumes?q=#{params[:search]}&maxResults=15&key=#{ENV["GOOGLE_API"]}"
+   user_serialized = open(url).read
+   user = JSON.parse(user_serialized)
+
+     title = "#{user['items'][0]['volumeInfo']['title']}"
+     kind = "Kind: #{user['kind']}"
+     author "Author: #{user['items'][0]['volumeInfo']['authors'].join}"
+     description = "Description: #{user['items'][0]['volumeInfo']['description']}"
+
+     publisher = "Publisher: #{user['items'][0]['volumeInfo']['publisher']}"
+     date = "Date: #{user['items'][0]['volumeInfo']['publishedDate']}"
+     type = "Type: #{user['items'][0]['volumeInfo']['categories']}"
+     rating = "Avarage rating: #{user['items'][0]['volumeInfo']['averageRating']}"
+     image = "Image: #{user['items'][0]['volumeInfo']['imageLinks']['thumbnail']}"
+     language = "Language: #{user['items'][0]['volumeInfo']['language']}"
+     preview = "Preview: #{user['items'][0]['volumeInfo']['previewLink']}"
+
+     Book.new(
+      title: title,
+      kind: kind,
+      author: author,
+      description: description,
+      publisher: publisher,
+      date: date,
+      type: type,
+      rating: rating,
+      image: image,
+      language: language,
+      preview: preview
+      )
+
   end
 end
 
