03a61f96bc4b35a4b69e62bab32b3f06b3c12010
Lukasz <lukaszkkoziel@gmail.com>
2020-06-19 11:30:19 +0900

    fixed controllers, models and routes. Can add author

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,19 +1,10 @@
 class Book < ApplicationRecord
   belongs_to :author
-include Elasticsearch::Model
-include Elasticsearch::Model::Callbacks
-
-  def as_indexed_json(options = {})
-    self.as_json(
-      only: [:name, :isbn, :published_at, :pages],
-      include: {
-        author: {
-          only: [:first_name, :last_name]
-        }
-      }
-    )
-  end
-
+  has_many :covers
+  validates :title, uniqueness: true
+  validates :title, presence: true
+   validates :name, uniqueness: true
+  validates :name, presence: true
   # def self.search(search)
   #   if search
   #     book = Book.find_by(name: search)
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+   # root to: 'pages#home'
+   resources :authors
   devise_for :users
    root to: 'pages#home'
-    resources :authors, only: [:new, :index, :show, :delete] do
-      resources :books, only: [:index, :search, :new, :create]
+   resources :authors, only: [:new, :index, :show, :create, :delete] do
+    resources :books, only: [:new, :create, :delete, :show]
 
-         end
+   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
