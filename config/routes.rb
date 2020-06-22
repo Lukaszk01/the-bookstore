636c8dab9fcded2b8a9a71a9db402bbdc99ae551
Lukasz <lukaszkkoziel@gmail.com>
2020-06-22 13:15:07 +0900

    dashboard route, controller and index created

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
    root to: 'pages#home'
    resources :authors, only: [:new, :index, :show, :create, :delete]
    resources :books, only: [:new, :create, :delete, :show]
+   resources :dashboard, only: [:index]
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
